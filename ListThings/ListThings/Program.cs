24f7622485a780cc53fa2e56778f24baae723abe
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2021-05-26 05:34:38 -0400

    Add files via upload

---
+++ ListThings/ListThings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListThings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new Employee[]
+            {
+                new Employee{ Name = "Jasmine"},
+                new Employee{ Name = "Lily"}
+            };
+
+            foreach(var employee in employees)
+            {
+                Console.WriteLine(employee.Name);
+            }
+            for(int i=0; i<employees.Count; i++)
+            {
+                Console.WriteLine(employees[i].Name);
+            }
+            Array.Resize(ref employees, 10);
+        }
+    }
+}
